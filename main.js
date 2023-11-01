52b5f4611c15fc7771645c1c425fc7708f13ccad
Thanh <thanh1705aurai@gmail.com>
2023-11-01 18:03:32 +0700

    finished modal login

--- main.js
+++ main.js
@@ -2,7 +2,26 @@ let $ = document.querySelector.bind(document)
 let $$ = document.querySelectorAll.bind(document)
 
 {
+  // hien modal
   let loginBtn = $(".accounts")
+  let loginModal = $(".modal-in")
   loginBtn.onclick = function() {
+    loginModal.classList.add("add")
+  }
+
+  // xoa modal
+  function remove() {
+    loginModal.classList.remove("add")
+  }
+
+  let closeBtn = $(".close-form__icon")
+  closeBtn.addEventListener("click", remove)
+
+  loginModal.onclick = function() {
+    this.addEventListener("click", remove)
+  }
+
+  $(".modal-in__content").onclick = function(e) {
+    e.stopPropagation()
   }
 }
